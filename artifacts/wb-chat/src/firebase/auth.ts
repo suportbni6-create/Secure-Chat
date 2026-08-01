b50f4a3be86c49d63c1f35792a7d0872da0525c9
suportbni6-create <suportbni6@gmail.com>
2026-08-01 22:25:56 +0900

    Delete artifacts directory

--- artifacts/wb-chat/src/firebase/auth.ts
+++
@@ -1,9 +0,0 @@
-import { signInAnonymously, signOut as firebaseSignOut, onAuthStateChanged as firebaseOnAuthStateChanged, User } from 'firebase/auth';
-import { auth } from './config';
-
-export const signInAnonymouslyUser = () => signInAnonymously(auth);
-export const signOut = () => firebaseSignOut(auth);
-
-export const onAuthStateChanged = (callback: (user: User | null) => void) => {
-  return firebaseOnAuthStateChanged(auth, callback);
-};
